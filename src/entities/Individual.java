ad6260c772fc5cf90b8777dad85bd97dde92402f
Joao Lucas <jldecristo@gmail.com>
2025-08-09 09:36:27 -0300

    classe Individual

--- src/entities/Individual.java
+++ src/entities/Individual.java
@@ -1,4 +1,23 @@
 package entities;
 
-public class Individual {
+public class Individual extends TaxPayer {
+
+    private Double healthSpending;
+
+    public Individual() {
+    }
+
+    public Individual(String name, Double annualIncome, Double healthSpending) {
+        super(name, annualIncome);
+        this.healthSpending = healthSpending;
+    }
+
+    public Double getHealthSpending() {
+        return healthSpending;
+    }
+
+    public void setHealthSpending(Double healthSpending) {
+        this.healthSpending = healthSpending;
+    }
+
 }
