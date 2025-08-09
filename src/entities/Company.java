badf3756e226996fa5621ab7bc08d480de0ddd00
Joao Lucas <jldecristo@gmail.com>
2025-08-09 09:50:40 -0300

    construtores com sobrecarga

--- src/entities/Company.java
+++ src/entities/Company.java
@@ -4,6 +4,12 @@ public class Company extends TaxPayer {
 
     private Integer employeesNumber;
 
+    public Company() {
+    }
 
+    public Company(String name, Double annualIncome, Integer employeesNumber) {
+        super(name, annualIncome);
+        this.employeesNumber = employeesNumber;
+    }
 
 }
