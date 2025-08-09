9348fba562940b54a3fc8a145a90729962ed3108
Joao Lucas <jldecristo@gmail.com>
2025-08-09 09:28:38 -0300

    TaxPayer

--- src/entities/TaxPayer.java
+++ src/entities/TaxPayer.java
@@ -5,5 +5,14 @@ public class TaxPayer {
     private String name;
     private Double annualIncome;
 
+    public TaxPayer() {
+    }
+
+    public TaxPayer(String name, Double annualIncome) {
+        this.name = name;
+        this.annualIncome = annualIncome;
+    }
+
+
 
 }
